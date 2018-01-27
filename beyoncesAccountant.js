46c237353bafe8195d174fbc9ded90df3b013117
Marin Cabac <marin.cabac@gmail.com>
2018-01-27 06:52:47 +0000

    automatically backed up by learn

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -4,7 +4,7 @@ console.log("Dear Beyonce,")
 console.log("We have calculated your total earnings this year.")
 console.log("You have earned " + beyoncesMoney)
 console.log("Of " + beyoncesMoney +" some portion derives from your live performances.")
-console.log("Of ${beyoncesMoney} some portion derives from music sales.")
+console.log("Of " + beyoncesMoney +" some portion derives from music sales.")
 
 
 // DON'T DO THIS PART YET:
